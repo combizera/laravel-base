d5007e19f42801fca58c30dc11b84eaf35dac2d8
Ygor Combi <ycombi349@gmail.com>
2023-11-10 18:40:57 -0300

    FEAT: call imgs and font-family in elements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Vite;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   */
+  public function register(): void
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-    }
+  /**
+   * Bootstrap any application services.
+   */
+  public function boot(): void
+  {
+    Vite::macro('image', fn (string $asset) => $this->asset("resources/img/{$asset}"));
+  }
 }
