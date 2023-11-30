6e220d2ae21463b5baaedba4c3ae70e3347524c3
Ygor Combi <ycombi349@gmail.com>
2023-11-30 17:04:23 -0300

    FEAT: organizar header

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,23 +4,24 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="icon" type="image/png" href="svg/favicon.png">
+
+    {{-- TÍTULO --}}
     <title>{{ $title ?? config('app.name') }}</title>
     @vite('resources/css/app.scss')
 
-    <!-- Google Tag Manager -->
+    {{-- PALAVRAS CHAVE --}}
+    <meta name="keywords" content="Key-word 1, Key-word 2, Key-word 3" />
 
-    <!-- End Google Tag Manager -->
-
+    {{-- TIPO --}}
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;700&display=swap" rel="stylesheet" />
 
+    {{-- FAVICON --}}
     <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
   </head>
   <body @isset($bodyClass) @class($bodyClass) @endisset>
-    <!-- Google Tag Manager (noscript) -->
-
-    <!-- End Google Tag Manager (noscript) -->
 
     {{-- HEADER --}}
     <x-header></x-header>
